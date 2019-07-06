2745e26596ffe63b5fa0632207fdcdd999051307
Ben Cooley <44032221+be-cool@users.noreply.github.com>
2019-07-06 14:03:30 -0400

    Bc/submit btn (#22)
    * test
    
    * minor html changes
    
    * merging changes
    
    * getting back to master
    
    * merged master
    
    * pulled in data from eventful and working to push it into cards onto the page

--- assets/javascript/eventful.js
+++ assets/javascript/eventful.js
@@ -1,10 +1,16 @@
 
+$("#submit-button").on("click", function(event){
+    event.preventDefault();
 //  Eventful API config:
 function efConfig() {
-    console.log(1)
+    // console.log(1)
     var efKey = "wDFFpKBkf7q72vwh";
-    var efLocation = "San Diego"
+    var efLocation = $("#location-input").val();
     var efQueryURL = "http://api.eventful.com/json/events/search?app_key=" + efKey + "&location=" + efLocation;
+
+    // console.log(efQueryURL);
+
+// console.log(efLocation);
 
 
     $.ajax({
@@ -15,15 +21,48 @@ function efConfig() {
         .then(function (response) {
             console.log(response);
 
-            var event = {
-                title: response.events.event[0].title,
-                city: response.events.event[0].city_name,
-                url: response.events.event[0].url,
-                image: response.events.event[0].image.small.url,
+            for (i=0; i < 12; i++) {
+                var event = {
+                    title: response.events.event[i].title,
+                    city: response.events.event[i].city_name,
+                    url: response.events.event[i].url,
+                    image: response.events.event[i].image.medium.url,
+                    // image: response.events.event[i].image
+                }
+                // 1. Make Card Div
+                var eventCard = $("<div class='card'>");
+                // var eventCard = $("<h5>Hello Wolrd</h5>");
+                var eventCardBody = $("<div class='card-body col-sm-3'>");
+                $("#data").append(eventCard);
+                eventCard.append(eventCardBody);
+
+
+                
+                // Loop through Object Attributes, to get display card data
+                for (key in event) {
+                    // Add title
+                    if (key == 'title') {
+                        var cardTitle = $("<h5 class='card-title'>");
+                        cardTitle.text(event[key]);
+                        eventCardBody.append(cardTitle);
+
+                    }
+                    else if (key == 'image') {
+                        var cardImage = $("<img class='card-img-top'>").attr("src", event[key]);
+                        eventCard.append(cardImage)
+
+                    }
+
+                    // console.log("Loop Counter" + key + " - " + event[key])
+                }
             }
-            console.log(event);
 
+            // console.log(event);
 
+            // $("#data").text(JSON.stringify(response));
+            // for (key in event) {
+            //     console.log("Loop Counter" + key + " - " + event[key])
+            // }
             //  Data within response that we could use: (events.event.<data>)
             //      - title
             //      - description
@@ -37,4 +76,4 @@ function efConfig() {
         });
 
 };
-efConfig();
+efConfig()});
