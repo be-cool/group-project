3d40168b005891dfb04defb8d4e3fbbbb95bae47
David Morales <dmoralesx24@gmail.com>
2019-07-06 13:24:51 -0400

    I added firebase code for the searches and put the required links in the html for it and jqery CDN

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,38 @@
+var config = {
+    apiKey: "AIzaSyBjA3IN-109oI4w7mQV83I2TXV6sNIO64g",
+    authDomain: "group-project1-aa81a.firebaseapp.com",
+    databaseURL: "https://group-project1-aa81a.firebaseio.com",
+    projectId: "group-project1-aa81a",
+    storageBucket: "",
+    messagingSenderId: "1002328856448",
+    appId: "1:1002328856448:web:60a681cb08c0bc50"
+  };
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
+  $("#submit-button").on("click", function(event) {
+    event.preventDefault();
+    var dateSearch = $("#example-date-input").val().trim();
+    var citySearch = $("#inputGroupSelect04").val().trim();
+  
+    var findBtn = {
+     date: dateSearch,
+     location: citySearch
+    };
+    database.ref("/user-search").push(findBtn);
+    console.log(findBtn.date);
+  });
+  
+  database.ref().on("child_added", function(childSnapshot) {
+      console.log(childSnapshot.val());
+     var going = childSnapshot.val().attending;
+     console.log(going);
+     $("#going-btn").append(going);
+     
+    },
+    function(errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    }
+  );
