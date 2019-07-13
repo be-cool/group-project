9ef6a4eafcccf10f249e2bdbc7f9407739e9ac7a
djfalciani <dfalciani@gmail.com>
2019-07-12 21:44:57 -0400

    Added clear function, hooked up quick city search and null check

--- assets/javascript/eventful.js
+++ assets/javascript/eventful.js
@@ -30,26 +30,25 @@ $("#submit-button").on("click", function (event) {
     event.preventDefault();
     //  Eventful API config:
     function efConfig() {
-        // console.log(1)
         var efKey = "wDFFpKBkf7q72vwh";
         var efLocation = $("#location-input").val();
-        if (efLocation === "") {
-            alert("enter a city");
-        }
-
         var efDate = startDate + "00" + "-" + endDate + "00";
-
         //  Supported: "All", "Future", "Past", "Today", "Last Week", "This Week", "Next week", 
         //  and months by name, e.g. "October". Exact ranges can be specified the form 'YYYYMMDD00-YYYYMMDD00', 
         //  for example '2012042500-2012042700';    ---this is a date range, will need to integrate search to ask for range---
         //  the last two digits of each date in this format are ignored.
-
-
+        
         //  Current date search value is in format 
-
         var efQueryURL = "http://api.eventful.com/json/events/search?app_key=" + efKey + "&location=" + efLocation + "&t=" + efDate;
-
-        console.log(efQueryURL);
+        // console.log(efQueryURL);
+        
+        // If City is Null exit...
+        if (efLocation === "") {
+            // console.log("Exit Eventful Call");
+            return;
+        }
+        // Clear any previously loaded Cards...
+        clearEventfulCards();
 
         $.ajax({
             url: efQueryURL,
@@ -116,4 +115,103 @@ $("#submit-button").on("click", function (event) {
     efConfig()
 });
 
+function clearEventfulCards() {
+    $("#data2").empty();
+}
 
+$(document).on("click", ".quick-search", QuickSearch);
+
+function QuickSearch() {
+    // 1. Get the City
+    var city = $(this).attr("data-city");
+    
+    // 2. CLear out any existing Cards...
+    clearEventfulCards();
+    // Manually clearing TIcketMaster Div. Not sure best practice regarding calling function in another JS file...
+    $("#data").empty();
+
+    // 3. Construct Query
+    var ApiQuery = queryBuilderQuickSearch(city);
+    console.log(ApiQuery);
+
+    if (ApiQuery === "") {
+        // console.log("Exit Eventful Call");
+        return;
+    }
+
+    // 4. render cards...
+    $.ajax({
+        url: ApiQuery,
+        dataType: 'jsonp',
+        method: "GET"
+    })
+        .then(function (response) {
+            for (i = 0; i < 10; i++) {
+                var eventfulData = {
+                    title: response.events.event[i].title,
+                    city: response.events.event[i].city_name,
+                    url: response.events.event[i].url,
+                    image: response.events.event[i].image === null ? 'assets/css/images/image.png': response.events.event[i].image.medium.url,
+                    startTime: response.events.event[i].start_time
+                }
+
+                // 1. Make Card Div
+                var eventCard = $("<div class='card col-sm-3'>");
+                var eventCardBody = $("<div class='card-body'>");
+
+                $("#data2").append(eventCard);
+                
+                // Loop through Object Attributes, to get display card data
+                for (key in eventfulData) {
+                    if (key == 'title') {
+                        var cardTitle = $("<h5 class='card-title'>");
+                        cardTitle.text(eventfulData[key]);
+                        eventCardBody.append(cardTitle);
+                        
+                    }
+                    else if (key == 'image') {
+                        var cardImage = $("<img class='card-img-top img-fluid'>").attr("src", eventfulData[key]);
+                        eventCard.append(cardImage)
+                        
+                    }
+                    else if (key =="city") {
+                        var cardCity = $('<h6 class="card-subtitle mb-2 text-muted"></h6>');
+                        cardCity.text(eventfulData[key]);
+                        eventCardBody.append(cardCity);
+                    }
+                    else if (key == "startTime") {
+                        var cardStartTime = $('<p class="card-text"></p>');
+                        var momentStartTime = moment(eventfulData[key]).format("llll");
+                        cardStartTime.text(momentStartTime);
+                        eventCardBody.append(cardStartTime);
+                    }
+                    
+                }
+                
+                // Append CardBody after Image is added...
+                eventCard.append(eventCardBody);
+            }
+
+        });
+
+}
+
+function queryBuilderQuickSearch(city) {
+    switch(city) {
+        case "Philadelphia":
+            return "http://api.eventful.com/json/events/search?app_key=wDFFpKBkf7q72vwh&location=Philadelphia"
+            break;
+        case "DC":
+            return "http://api.eventful.com/json/events/search?app_key=wDFFpKBkf7q72vwh&location=Washington%20DC"
+          break;
+        case "Boston":
+            return "http://api.eventful.com/json/events/search?app_key=wDFFpKBkf7q72vwh&location=Boston"
+            break;
+        case "NYC":
+            return "http://api.eventful.com/json/events/search?app_key=wDFFpKBkf7q72vwh&location=New%20York%20City"
+            break;
+        default:
+            return ""
+      }
+    
+}
